1c185d7a248e79b91c8bf243eb89cb4ab2a7b90b
jhagle4 <haglejimmie@gmail.com>
2019-07-19 12:46:05 -0700

    ..

--- pipelineTest/src/main/java/com/revature/web/MarketTaBullControllerImpl.java
+++ pipelineTest/src/main/java/com/revature/web/MarketTaBullControllerImpl.java
@@ -4,10 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.service.UserService;
@@ -24,8 +22,7 @@ public class MarketTaBullControllerImpl implements MarketTaBullController{
 	}
 	
 	@Override
-	@PostMapping(value="/login", produces=MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
+	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public void isValidUser(HttpServletRequest req, HttpServletResponse resp) {
 		us.isValidUser(req, resp);
 		
