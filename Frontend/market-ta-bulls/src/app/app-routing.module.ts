189c8d4fd543fdce5167843ea3d1a986efd97009
andreb123 <andrescottbush@hotmail.com>
2019-07-19 10:23:21 -0500

    addded page components and login form

--- Frontend/market-ta-bulls/src/app/app-routing.module.ts
+++ Frontend/market-ta-bulls/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { Routes, RouterModule } from '@angular/router';
+import { StockpickerComponent } from './component/stockpicker/stockpicker.component';
+import { SettingComponent } from './component/setting/setting.component';
+
+const routes: Routes = [{
+  component: PortfolioComponent,
+  path: "portfolio"},{
+  component: StockpickerComponent,
+  path: "stockpicker"},{
+  component: SettingComponent,
+  path: "setting"
+}];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   declarations: []
 })
 export class AppRoutingModule { }
--- Frontend/market-ta-bulls/src/app/app.module.ts
+++ Frontend/market-ta-bulls/src/app/app.module.ts
@@ -4,12 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './component/header/header.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { StockpickerComponent } from './component/stockpicker/stockpicker.component';
+import { SettingComponent } from './component/setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    PortfolioComponent,
+    StockpickerComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
