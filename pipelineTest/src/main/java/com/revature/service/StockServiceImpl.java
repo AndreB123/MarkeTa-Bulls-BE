5d978b98b87d4b148d1937869afff072df70dddd
dkhalifa95 <doaak.k95@gmail.com>
2019-07-20 18:47:30 -0400

    StockContoller and StockContollerImpl

--- pipelineTest/src/main/java/com/revature/service/StockServiceImpl.java
+++ pipelineTest/src/main/java/com/revature/service/StockServiceImpl.java
@@ -1,8 +1,33 @@
 package com.revature.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.model.Stock;
+import com.revature.repository.StockRepository;
+import com.revature.repository.StockRepositoryImpl;
+
 @Service("ss")
-public class StockServiceImpl {
+public class StockServiceImpl implements StockService {
+
+	private StockRepositoryImpl sr2;
+
+	@Autowired
+	public StockServiceImpl(StockRepository sr) {
+		this.sr2 = sr2;
+	}
+
+	@Override
+	public List<Stock> getAllStocks(HttpServletRequest req, HttpServletResponse resp) {
+		HttpSession s = req.getSession(false);
+		int portfolioId = Integer.parseInt((String)s.getAttribute("portfolioid"));
+		return new StockRepositoryImpl().getAllStocks(portfolioId);
+	}
 
 }
--- pipelineTest/src/main/java/com/revature/web/StockControllerImpl.java
+++ pipelineTest/src/main/java/com/revature/web/StockControllerImpl.java
@@ -1,5 +1,31 @@
 package com.revature.web;
 
-public class StockControllerImpl {
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.model.Stock;
+import com.revature.service.StockService;
+
+public class StockControllerImpl implements StockController {
+
+	private StockService ss;
+
+	@Autowired
+	public void setSs(StockService ss) {
+		this.ss = ss;
+	}
+
+	@Override
+	@RequestMapping(value = "/stockpage", method = RequestMethod.GET)
+	public List<Stock> getAllStocks(HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println("stock controller method");
+		return ss.getAllStocks(req, resp);
+	}
 
 }
