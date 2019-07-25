55303a81ac03918f2f4037ed0c6922891327041d
andreb123 <andrescottbush@hotmail.com>
2019-07-25 09:13:03 -0500

    fixing imports

--- pipelineTest/src/main/java/com/revature/web/StockController.java
+++ pipelineTest/src/main/java/com/revature/web/StockController.java
@@ -2,9 +2,6 @@ package com.revature.web;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.revature.model.Stock;
 
 public interface StockController {
