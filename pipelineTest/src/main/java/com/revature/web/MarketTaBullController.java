838302042954cf7ae8b27af468d27b1df1d0f7a8
jhagle4 <haglejimmie@gmail.com>
2019-07-24 22:24:42 -0700

    Registration fixed

--- pipelineTest/src/main/java/com/revature/web/MarketTaBullController.java
+++ pipelineTest/src/main/java/com/revature/web/MarketTaBullController.java
@@ -5,6 +5,6 @@ package com.revature.web;
 public interface MarketTaBullController {
 	public String isValidUser(String username, String Password);	
 	public void getAllUsers(String username, String password);
-	public void createUser(String username, String password, double balance);
+	public void createUser(String username, String password, String balance);
 
 }
--- pipelineTest/src/main/java/com/revature/web/PortfolioControllerImpl.java
+++ pipelineTest/src/main/java/com/revature/web/PortfolioControllerImpl.java
@@ -3,10 +3,6 @@ package com.revature.web;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
