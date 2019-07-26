4a86b909c5e7d83acc6f7d95072abede582a8a39
jhagle4 <haglejimmie@gmail.com>
2019-07-26 04:25:23 -0700

    Test 100

--- pipelineTest/src/main/java/com/revature/web/MarketTaBullControllerImpl.java
+++ pipelineTest/src/main/java/com/revature/web/MarketTaBullControllerImpl.java
@@ -24,7 +24,7 @@ public class MarketTaBullControllerImpl implements MarketTaBullController{
 	@Override
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	@CrossOrigin(origins = "http://market-ta-bulls.s3-website-us-west-1.amazonaws.com")
-	public String isValidUser(@RequestParam(name="username") String username, @RequestParam(name="password") String password) {
+	public String isValidUser(@RequestParam(name="Username") String username, @RequestParam(name="password") String password) {
 		System.out.println("controller login method");
 		ObjectMapper mapper = new ObjectMapper();
 		try {
@@ -48,7 +48,7 @@ public class MarketTaBullControllerImpl implements MarketTaBullController{
 	@Override
 	@RequestMapping(value="/getAllUsers", method=RequestMethod.GET)
 	@CrossOrigin(origins = "http://market-ta-bulls.s3-website-us-west-1.amazonaws.com")
-	public void getAllUsers(@RequestParam(name="username") String username, @RequestParam(name="password") String password) {
+	public void getAllUsers(@RequestParam(name="Username") String username, @RequestParam(name="password") String password) {
 		System.out.println("controller get all users method");
 		us.getAllUsers(username, password);
 		return;		
